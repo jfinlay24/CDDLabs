9d75f8a7ba657571a24bdb80be0be7cc3940e223
jfinlay24 <jfinlay24@yahoo.ie>
2017-10-09 12:08:04 +0100

    2nd commit

---
+++ Lab 3/signal.cpp
@@ -0,0 +1,24 @@
+#include "Semaphore.h"
+#include <iostream>
+#include <thread>
+int count = 0;
+
+void taskOne(std::shared_ptr<Semaphore> mutex){
+  mutex->Wait();
+  count += 1;
+  mutex->Signal();
+  std::cout << "count\n";
+}
+
+int main(void){
+  std::thread threadOne, threadTwo;
+  std::shared_ptr<Semaphore> mutex( new Semaphore);
+  /**< Launch the threads  */
+  mutex->Signal();
+  threadOne=std::thread(taskOne, mutex);
+  threadTwo=std::thread(taskOne, mutex);
+  std::cout << "Launched from the main\n";
+  threadOne.join();
+  threadTwo.join();
+  return 0;
+}
